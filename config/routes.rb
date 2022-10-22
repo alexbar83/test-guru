cbd46e14ab0e8c83202585ba9bfd478cee187bdd
aleks <aleksandr.baryshnikov83@gmail.com>
2022-10-22 21:18:54 +0200

    about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'tests#index'
+
+  get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
+
+  resources :sessions, only: %i[create destroy]
+
+  resources :users, only: :create
+
+  resources :tests do
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true, except: :index
+    end
+    post :start, on: :member
+  end
+
+  resources :test_passages, only: %i[show update] do
+    get :result, on: :member
+  end
 end
