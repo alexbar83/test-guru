987e67563b712c582d9af808157293c733d67d59
aleks <aleksandr.baryshnikov83@gmail.com>
2022-10-22 21:38:03 +0200

    auth1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
-  #helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?
 
-  #def authenticate_user!
-   # unless current_user
-      #redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please'
-     # cookies[:path] = '/5b2685e8e672ae7f0b313dc85261b9e5'
+  def authenticate_user!
+    unless current_user
+      redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please'
+      cookies[:path] = '/5b2685e8e672ae7f0b313dc85261b9e5'
     end
   end
 
-  #def current_user
-   # @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  #end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 
-  #def logged_in?
-   # current_user.present?
-  #end
+  def logged_in?
+    current_user.present?
+  end
 end
