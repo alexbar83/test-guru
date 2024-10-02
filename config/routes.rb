788c926a525ba8bc91d128d37815c65198c6f622
alexbar83 <aleksandr.baryshnikov83@gmail.com>
2024-10-02 17:58:04 +0200

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
+ 
   root to: "tests#index" 
 
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
-  delete :logout, to: 'sessions#delete'
+  devise_for :users, controllers: { sessions: 'users/sessions' }, path: :gurus,
+                     path_names: { sign_in: :login, sign_out: :logout }
 
-  resources :users, only: :create
-  resources :sessions, only: :create
 
-  resources :tests, shallow: true, except: :index  do 
-    resources :questions, shallow: true,  except: :index do 
-      resources :answers 
-    end
+  resources :tests, only: :index do
     member do
       post :start
     end
@@ -20,6 +15,14 @@ Rails.application.routes.draw do
   resources :test_passings, only: %i[show update] do
     member do
       get :result
+    end
+  end
+
+  namespace :admin do
+    resources :tests do
+      resources :questions, shallow: true, except: :index do
+        resources :answers, shallow: true, except: :index
+      end
     end
   end
 end
