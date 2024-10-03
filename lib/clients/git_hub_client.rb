02a6e5df3b54d8b90ae4d9b710be6bf4ba2c1d99
alexbar83 <aleksandr.baryshnikov83@gmail.com>
2024-10-03 23:23:59 +0200

    gist

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,5 +1,5 @@
 class GitHubClient
-
+  ACCESS_TOKEN = "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"
   ROOT_ENDPOINT = 'https://api.github.com'
 
   def initialize
@@ -8,7 +8,7 @@ class GitHubClient
 
   def create_gist(params)
     @http_client.post('gists', params) do |request|
-      request.headers['Authorization'] = "token #{ENV['ACCESS_TOKEN']}"
+      request.headers['Authorization'] = ACCESS_TOKEN
       request.headers['Content-Type'] = 'application/json'
       request.body = params.to_json
     end
